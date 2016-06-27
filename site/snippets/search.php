12734661a59ee2dbc690688f8b6c0d55f6efb116
Matthieu Bergel <matthieu@grainedesite.fr>
2016-06-27 18:42:58 +0200

    Added clear search (ext search snippet)

---
+++ site/snippets/search.php
@@ -0,0 +1,14 @@
+<div id="search">
+  <div class="container">
+    <div class="search-box">
+      <span class="i-search"></span>
+      <div class="search-input"></div>
+      <a href="#" onClick="clear_search(event);" class="i-cancel-circle"></a>
+    </div>
+  </div>
+</div>
+
+
+<div class="container" id="search-hits">
+  <div class="content"></div>
+</div>
