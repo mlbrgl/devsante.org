b6d2288be7f9889cb7a66ca6eb41c0fbc99f0504
Matthieu Bergel <matthieu@grainedesite.fr>
2016-07-09 15:27:38 +0200

    Hiding communication ponctuelle block

--- site/templates/home.php
+++ site/templates/home.php
@@ -16,7 +16,8 @@
     </section>
 
 
-    <?php /* <section id="message" class="annexes">
+    <?php /* 
+    <section id="message" class="annexes">
       <h1 class="section-title"><span class="i-bullhorn"></span>Message</h1>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pellentesque commodo lorem consequat ullamcorper. Pellentesque nec placerat turpis.    Vivamus laoreet maximus nulla. Aenean pretium libero vel elit sodales, eget dapibus orci vulputate.
     </section> 
@@ -25,7 +26,8 @@
     <section id="numeros-speciaux" class="annexes">
       <h1 class="section-title"><span class="i-files-empty"></span>Numéros spéciaux</h1>
       Suspendisse. Donec sagittis malesuada quam et lobortis. Pellentesque id ornare sem. Phasellus consectetur dui ligula, quis lobortis nisi place
-    </section> */ ?>
+    </section> 
+    */ ?>
 
     <section id="actualites" class="annexes">
       <h1 class="section-title"><span class="i-newspaper"></span>Actualités</h1>
@@ -38,7 +40,7 @@
       Cras tincidunt purus fringilla mauris vehicula tempor. Vestibulum varius dignissim ligula, et iaculis urna rutrum molestie. Etiam et mollis mauris. tum eu dui nec, lobortis maximus ligula.
     </section>
     
-
+  <?php /*
   <section id="communication-ponctuelle">
     <div class="text">
       <div class="title">Emplacement ponctuel pour communiquer</div>
@@ -48,6 +50,7 @@
       <a href="/"><span>></span></a>
     </div>
   </section>
+  */?>
   
 
 </main>
