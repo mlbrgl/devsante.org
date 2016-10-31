b2dd5075ad49417fdb9372f8ea5278eedd6f77d8
Matthieu Bergel <matthieu@grainedesite.fr>
2016-10-31 21:26:18 +0100

    Cleanup + 301 redirects

--- site/controllers/error.php
+++ site/controllers/error.php
@@ -4,10 +4,20 @@ return function($site, $pages, $page) {
      
   $redirects_filepath = __DIR__ . '/../plugins/redirects/' . c::get('redirects')['redirects_filename'];
 
-  if(f::exists($redirects_filepath)) { 
-
+  // Not necessary - might be useful for performance, not checked
+  // // Optimistically try to locate the page by using the same slug as the previous site
+  // $slug = $site->kirby()->request()->path()->last();
+  // $new_page = $site->index()->visible()->find('articles/' . $slug, 'actualites/' . $slug)->first();
+  // if(!emtpy($new_page)) {
+  //   echo $new_page->title() . '<br>';
+  //   // go($new_page->url(), 301);
+  // } elseif (f::exists($redirects_filepath)) { 
+  // 
+  if (f::exists($redirects_filepath)) { 
+    
     // Get the path originally requested
     $path_requested = '/' . $site->kirby()->request()->path();
+    
     // Load the file containing the redirects
     $redirects = f::read($redirects_filepath);   
 
@@ -20,41 +30,13 @@ return function($site, $pages, $page) {
       
       if($redirect_src_dest[0] === $path_requested) {
         // If a matching redirect if found, go there
-        go($redirect_src_dest[1]);
+        go($redirect_src_dest[1], 301);
       } else {
         // Read the next src redirect
         $redirect = strtok(PHP_EOL);
       }
     }
   }
-
-
-
-  
-  // // Optimistically try to locate the page by using the same slug as the previous site
-  // $new_page = $site->index()->visible()->find('articles/' . $slug, 'actualites/' . $slug)->first();
-  
-  // if(empty($new_page)) {
-  //   print 'could not find it, starting searching ...' . '<br>';
-
-  //   $search_phrase = str_replace('-', ' ', $slug);
-  //   print 'search phrase: ' . $search_phrase  . '<br>';
-
-  //   // Search for the page based on its slug the page is being requested with
-  //   $new_pages = $site->index()->visible()->search($search_phrase, 'title'); 
-
-  //   foreach($new_pages as $new_page) {
-  //     echo $new_page->title() . '<br>';
-  //   }
-  // }
-
-  // // No guarantee that the search above actually found a record, so we still
-  // // need to check before redirecting.
-  // if(!empty($new_page)) {
-  //   print 'redirecting now ...' . '<br>';
-  //   echo $new_page->title() . '<br>';
-  //   // go($new_page->url(), 301);
-  // }
 
   return array();
 };
