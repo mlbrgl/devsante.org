119208f5866d58fed6dc84574c818fd54e38f87e
Matthieu Bergel <matthieu@grainedesite.fr>
2016-10-31 21:05:50 +0100

    Cleanup notice

--- site/snippets/top.php
+++ site/snippets/top.php
@@ -14,7 +14,7 @@
 
 
 </head>
-<body<?php ecco(isset($context), ' class="' . $context . '"');?>>
+<body<?php echo isset($context) ? ' class="' . $context . '"' : '';?>>
   
   <header class="header cf" role="banner">
     
