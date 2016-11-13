f3da0741cde556e65bd39033d112bbdf3608fd51
Matthieu Bergel <matthieu@grainedesite.fr>
2016-11-13 18:14:14 +0100

    Committing for archiving purposes. In the end, the redirections are computed when articles are being exported from Drupal. The redirects are then more deterministic.

--- site/plugins/redirects/redirects.php
+++ site/plugins/redirects/redirects.php
@@ -12,7 +12,8 @@ function redirects_generate_csv() {
   $index = $client->initIndex($algolia_settings['index']);
 
   // Set parameters for search
-  $query_params = ['attributesToRetrieve' => '_id', 
+  $query_params = ['attributesToRetrieve' => '_id,datetime',
+                   'restrictSearchableAttributes' => 'datetime',
                    'length' => 1,
                    'offset' => 0, // without 'offset', 'length' is not considered
                   ];
@@ -24,27 +25,38 @@ function redirects_generate_csv() {
   f::remove($redirects_filepath);
 
   // Read the src redirects, one line at a time
-  $redirect_src = strtok($redirects_src, PHP_EOL);
-  while($redirect_src != false) {
-    $search_phrase = str_replace('-', ' ', basename($redirect_src));
+  $redirect_src_url_date = strtok($redirects_src, PHP_EOL);
+  while($redirect_src_url_date != false) {
+    // Searching by keywords (not precise enough)
+    // $search_phrase = str_replace('-', ' ', basename($redirect_src_url_date));
     
+    // Searching by datetime, making the assumption that no 2 articles were written on the same day
+    // $query_params['filters' => 'datetime: ' . trim(explode(',', $redirect_src_url_date)[1])]; 
+    
+    $search_phrase = trim(explode(',', $redirect_src_url_date)[1]); 
+
     // Run the search against Algolia index
     $res = $index->search($search_phrase, $query_params);
 
-    // f::write($redirects_filepath, $redirect_src . ',' . $search_phrase . PHP_EOL, true);
-   
-    // Only write in the redirects file if Algolia returns a search result
-    if(isset($res['hits'][0]['_id'])) {
+    // f::write($redirects_filepath, $redirect_src_url_date . ',' . $search_phrase . PHP_EOL, true);
+    
+    $redirect_dest_url = ' ';
+    $redirect_dest_datetime = ' ';
 
+    if(isset($res['hits'][0]['_id'])) {
       // Algolia returns the full id, which contains the # to uniquely identify the paragraph
       $res_id = $res['hits'][0]['_id'];
-      $redirect_dest = explode('#', $res_id)[0];
-      
-      f::write($redirects_filepath, $redirect_src . ',' . $redirect_dest . PHP_EOL, true);
+      $redirect_dest_url = explode('#', $res_id)[0];
     }
 
+    if(isset($res['hits'][0]['datetime'])) {
+      $redirect_dest_datetime = date('d F Y', $res['hits'][0]['datetime']);
+    }
+
+    f::write($redirects_filepath, $redirect_src_url_date . ',' . $redirect_dest_url . ',' . $redirect_dest_datetime .  PHP_EOL, true);
+
     // Read the next src redirect
-    $redirect_src = strtok(PHP_EOL);
+    $redirect_src_url_date = strtok(PHP_EOL);
   }
 
 }
