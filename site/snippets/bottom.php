c50e2b6177e8045ddcbcd969e2edcf7754c9deb9
Matthieu Bergel <matthieu@grainedesite.fr>
2016-11-15 13:28:49 +0100

    Activated GA tracking

--- site/snippets/bottom.php
+++ site/snippets/bottom.php
@@ -46,7 +46,6 @@
     })(); 
   </script>
   
-  <?php /*
   <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -57,8 +56,6 @@
     ga('send', 'pageview');
 
   </script>
-  */
-  ?>
 
 </body>
 </html>
