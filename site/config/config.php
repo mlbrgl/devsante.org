f4cffdc79c29860dd5f1ee7f104230c0bfb872f0
Matthieu Bergel <matthieu@grainedesite.fr>
2016-12-18 17:45:38 +0100

    Updated locale (utf8)

--- site/config/config.php
+++ site/config/config.php
@@ -14,7 +14,7 @@ of the system, please check out http://getkirby.com/docs/advanced/options
 
 // Switch date handler for translated months and days
 c::set('date.handler', 'strftime'); 
-c::set('locale', 'fr_FR');
+c::set('locale', 'fr_FR.utf8');
 //c::set('markdown.extra', true);
 
 // Timezone setting
