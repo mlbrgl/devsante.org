9078247cceeaa4245916433193f53ef21fea37bb
Matthieu Bergel <matthieu@grainedesite.fr>
2017-03-03 14:53:40 +0100

    HP: hover on entire block (vs title only)

--- site/snippets/excerpt.php
+++ site/snippets/excerpt.php
@@ -4,22 +4,21 @@ if(isset($excerpt) || isset($excerpturi) && $excerpt = page($excerpturi)):
 ?>
 
   <article>
-    <h1>
-      <a href="<?php echo $excerpt->url() ?>">
-        <?php echo html($excerpt->title()) ?>
-      </a>
-    </h1>
-    
-    <div class="meta">
-      <?php if(!empty($excerpt->author())) : ?>
-        <div class="auteur"><?php echo html($excerpt->author()) ?></div>
-      <?php endif; ?>
-    </div>
-
-    <div class="teaser">
-      <p><?php echo $excerpt->text()->excerpt(300) ?></p>
-    </div>
-
+    <a href="<?php echo $excerpt->url() ?>">
+      <h1><?php echo html($excerpt->title()) ?></h1>
+      
+      <div class="meta">
+        <?php if(!empty($excerpt->author())) : ?>
+          <div class="auteur"><?php echo html($excerpt->author()) ?></div>
+        <?php endif; ?>
+      </div>
+      
+      <div class="teaser">
+        <p><?php echo $excerpt->text()->excerpt(300) ?></p>
+      </div>
+      
+    </a>
+      
     <div class="date"><time datetime="<?php echo $excerpt->datetime() ?>"> <?php echo html($excerpt->date('%B %Y','datetime')) ?> </time></div>
 
   </article>
--- site/snippets/home-latest-articles.php
+++ site/snippets/home-latest-articles.php
@@ -16,17 +16,18 @@ $last = $latest_articles->last();
 
 <?php foreach($latest_articles as $article): ?>
   <article<?php ecco($article == $last,' class="last"') ?>>
-    <h1>
-      <a href="<?php echo $article->url() ?>">
-        <?php echo html($article->title()) ?>
-      </a>
-    </h1>
-    <div class="meta">
-      <div class="author"><?php echo html($article->author()) ?></div>
-      <div class="date"><time datetime="<?php echo $article->datetime() ?>"> <?php echo html($article->date('%d %B %Y','datetime')) ?> </time></div>
-    </div>
-    <div class="teaser">
-      <p><?php echo html($article->teaser()->excerpt(300)) ?></p>
-    </div>
+    <a href="<?php echo $article->url() ?>">
+      <h1><?php echo html($article->title()) ?></h1>
+      
+      <div class="meta">
+        <div class="author"><?php echo html($article->author()) ?></div>
+        <div class="date"><time datetime="<?php echo $article->datetime() ?>"> <?php echo html($article->date('%d %B %Y','datetime')) ?> </time></div>
+      </div>
+      
+      <div class="teaser">
+        <p><?php echo html($article->teaser()->excerpt(300)) ?></p>
+      </div>
+      
+    </a>
   </article>
 <?php endforeach ?>
