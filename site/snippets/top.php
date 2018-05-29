99ccf308cd616e218907624d7dfd7cad9d3544bf
Matthieu Bergel <hello@matthieubergel.org>
2018-05-29 17:43:18 +0200

    Added favicons

--- site/snippets/top.php
+++ site/snippets/top.php
@@ -9,6 +9,18 @@
   <meta name="description" content="<?php echo $site->description()->html() ?>">
   <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
 
+  <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicons/apple-touch-icon.png?v=694NrnM4Ex">
+  <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicons/favicon-32x32.png?v=694NrnM4Ex">
+  <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicons/favicon-16x16.png?v=694NrnM4Ex">
+  <link rel="manifest" href="/assets/favicons/site.webmanifest?v=694NrnM4Ex">
+  <link rel="mask-icon" href="/assets/favicons/safari-pinned-tab.svg?v=694NrnM4Ex" color="#365086">
+  <link rel="shortcut icon" href="/assets/favicons/favicon.ico?v=694NrnM4Ex">
+  <meta name="apple-mobile-web-app-title" content="Devsante">
+  <meta name="application-name" content="Devsante">
+  <meta name="msapplication-TileColor" content="#2b5797">
+  <meta name="msapplication-config" content="/assets/favicons/browserconfig.xml?v=694NrnM4Ex">
+  <meta name="theme-color" content="#365086">
+
   <?php echo css('assets/css/app.css') ?>
 
 </head>
