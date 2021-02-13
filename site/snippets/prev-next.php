bf35290285224c568298f70271f91c06c79bd20b
Matthieu Bergel <hello@matthieubergel.org>
2021-02-13 17:58:06 +0100

    fix(pagination): reverse next / previous on pages too

--- site/snippets/prev-next.php
+++ site/snippets/prev-next.php
@@ -1,11 +1,11 @@
 <nav class="excerpts" role="navigation">
-  <?php if($prev = $page->prevVisible()) : ?>
+  <?php if($prev = $page->nextVisible()) : ?>
     <div class="excerpt previous">
       <div class="subhead"><a href="<?php echo $prev->url(); ?>"><span class="icon i-arrow-left"></span><?php echo $label_previous ?></span></a></div>
       <?php snippet('excerpt-news', array('excerpt' => $prev)); ?>
     </div>
   <?php endif; ?>
-  <?php if($next = $page->nextVisible()) : ?>
+  <?php if($next = $page->prevVisible()) : ?>
     <div class="excerpt next">
       <div class="subhead"><a href="<?php echo $next->url(); ?>"><?php echo $label_next ?></a><span class="icon i-arrow-right"></span></a></div>
       <?php snippet('excerpt-news', array('excerpt' => $next)); ?>
