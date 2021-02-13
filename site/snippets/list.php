f7e74baeedaaa7c024d0196a9965ee200375c9aa
Matthieu Bergel <hello@matthieubergel.org>
2021-02-13 11:46:01 +0100

    fix(list): restore

--- site/snippets/list.php
+++ site/snippets/list.php
@@ -2,8 +2,10 @@
 
 <?php foreach($articles as $article): ?>
   <article>
-    <h1><a href="<?= $article->url() ?>"><?php echo $article->title()->html(); ?></a></h1>
-    <div class="date"><?php echo $article->date('%d %B %Y', 'datetime') ?></div>
+    <div class="title-date">
+      <h1><a href="<?= $article->url() ?>"><?php echo $article->title()->html(); ?></a></h1>
+      <div class="date"><?php echo $article->date('%d %B %Y', 'datetime') ?></div>
+    </div>
     <div class="author"><?php echo $article->author()->html(); ?></div>
     <div class="text"><?php echo $article->text()->kt()->excerpt(300); ?></div>
   </article>
