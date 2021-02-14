66339da1005bbec02643016ae49b143046f6e554
Matthieu Bergel <hello@matthieubergel.org>
2021-02-14 11:59:05 +0100

    enhance(notifications): more visible CTA

--- site/patterns/cookie-bar/cookie-bar.html.php
+++ site/patterns/cookie-bar/cookie-bar.html.php
@@ -1,6 +1,8 @@
 <div id="cookie-bar">
   <div class="container">
       <div class="text">Ce site utilise des cookies pour le suivi d'audience et la personnalisation des contenus.</div>
-      <div class="cta"><a href="">J'accepte</a></div>
+      <div class="cta">
+        <a class="ok" href="">J'accepte</a>
+      </div>
   </div>
 </div>
--- site/patterns/survey/survey.html.php
+++ site/patterns/survey/survey.html.php
@@ -2,8 +2,8 @@
   <div class="container">
       <div class="text">Avez-vous deux minutes pour nous aider à améliorer le site ?</div>
       <div class="cta">
-        <div class="ok"><a target="_blank" href="https://docs.google.com/forms/d/1ZVW2mSW0i9Sx0Ig3xpYEWOPZZZHx-a-XXU9FaSWkIYo/viewform">OK</a></div>
-        <div class="later"><a href="">Plus tard</a></div>
+        <a class="later" href="">Plus tard</a>
+        <a class="ok" target="_blank" href="https://docs.google.com/forms/d/1ZVW2mSW0i9Sx0Ig3xpYEWOPZZZHx-a-XXU9FaSWkIYo/viewform">OK</a>
       </div>
   </div>
 </div>
