4a2407efc5fedda4a1174dca05cd00b07192353e
Matthieu Bergel <hello@matthieubergel.org>
2021-03-14 22:39:28 +0100

    fix(kirby3): date in list

--- site/snippets/list/list.php
+++ site/snippets/list/list.php
@@ -4,7 +4,7 @@
   <article>
     <div class="title-date">
       <h1><a href="<?= $article->url() ?>"><?php echo $article->title()->html(); ?></a></h1>
-      <div class="date"><?php echo $article->date('%d %B %Y', 'datetime') ?></div>
+      <div class="date"><?php echo $article->datetime()->toDate('%d %B %Y') ?></div>
     </div>
     <div class="author"><?php echo $article->author()->html(); ?></div>
     <div class="text"><?php echo $article->text()->kt()->excerpt(300); ?></div>
