25423c1623300432d6e019d1aa3bf8913a74e64c
Matthieu Bergel <hello@matthieubergel.org>
2021-03-20 11:14:40 +0100

    chore(gulp): update default task to new API

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,7 @@ var min = {
   js: 'assets/js/app.js'
 }
 
-gulp.task('connect-sync', function() {
+function connectSync() {
   const config = {router: 'kirby/router.php'}
   // php.ini used for xdebug configuration
   if(existsSync('./php.ini')) config.ini = './php.ini'
@@ -40,28 +40,27 @@ gulp.task('connect-sync', function() {
     });
   });
 
-  gulp.watch(["assets/css/src/*.scss",'site/snippets/**/*.scss'], gulp.series('css'));
-  gulp.watch('assets/js/src/**/*.js', gulp.series('js'));
+  gulp.watch(["assets/css/src/*.scss",'site/snippets/**/*.scss'], {ignoreInitial: false}, css);
+  gulp.watch('assets/js/src/**/*.js', {ignoreInitial: false}, js);
   gulp.watch(src.php).on('change', browserSync.reload);
+};
 
-});
-
-gulp.task('css', function () {
+function css() {
   return gulp.src(src.css)
     .pipe(sass().on('error', sass.logError))
     .pipe(concat('app.css'))
     .pipe(cleanCSS())
     .pipe(gulp.dest(dist.css))
     .pipe(browserSync.stream());
-});
+};
 
-gulp.task('js', function () {
+function js() {
   return gulp.src(src.js)
     .pipe(gulpif('!**/*.min.js', uglify())) // only uglify() files that have not been minified yet.
     .pipe(concat('app.js'))
     .pipe(gulp.dest(dist.js))
     .pipe(browserSync.stream());
-});
+};
 
 gulp.task('cachebust-css', function(){
   return gulp.src(min.css)
@@ -77,5 +76,5 @@ gulp.task('cachebust-js', function(){
 });
 
 
-gulp.task("default", gulp.series("css", "js", "connect-sync"));
+exports.default = connectSync;
 gulp.task("prod", gulp.series("cachebust-css", "cachebust-js"));
