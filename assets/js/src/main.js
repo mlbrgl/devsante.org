8a043c9128c1e3e18f6dee0ce25755b18c98d950
Matthieu Bergel <hello@matthieubergel.org>
2021-04-19 09:17:50 +0200

    style: prettify main.js

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -1,7 +1,7 @@
 // Webfont load
 WebFont.load({
-  google: { families: [ 'Lato:400,700,400italic:latin' ] },
-  active: renderMasonry
+  google: { families: ["Lato:400,700,400italic:latin"] },
+  active: renderMasonry,
 });
 
 // Algolia variables are set in bottom.php
@@ -14,8 +14,8 @@ var search = instantsearch({
   // searchFunction: _.debounce(search_func, 200),
   searchFunction: search_func,
   searchParameters: {
-    distinct: 2
-  }
+    distinct: 2,
+  },
 });
 
 var searchBox = instantsearch.widgets.searchBox({
@@ -26,22 +26,24 @@ var searchBox = instantsearch.widgets.searchBox({
 });
 
 var hits = instantsearch.widgets.hits({
-  container: '#search-hits .content',
-  transformItems: function(items) {
+  container: "#search-hits .content",
+  transformItems: function (items) {
     return items.map(function (item) {
       const updatedItem = Object.assign({}, item);
-      
+
       // Removing metadata for subsequent paragraphs in the search
-      if(item._distinctSeqID !== 0) {
+      if (item._distinctSeqID !== 0) {
         delete updatedItem._highlightResult.title;
         delete updatedItem.author;
         delete updatedItem.datetime;
       } else {
-        updatedItem.datetime = get_locale_date_string_fallback(item.datetime * 1000);
+        updatedItem.datetime = get_locale_date_string_fallback(
+          item.datetime * 1000
+        );
       }
 
       return updatedItem;
-    })
+    });
   },
   templates: {
     item:
@@ -68,10 +70,10 @@ var hits = instantsearch.widgets.hits({
 
 // Pagination widget
 var pagination = instantsearch.widgets.pagination({
-  container: '#search-pagination',
+  container: "#search-pagination",
   maxPages: 20,
   showFirstLast: false,
-  padding: 2
+  padding: 2,
 });
 
 search.addWidgets([searchBox, hits, pagination]);
@@ -86,21 +88,19 @@ search.addWidgets([searchBox, hits, pagination]);
 
 search.start();
 
-
 /**
  * Search callback
  *
  * @param      {string}  helper  The helper
  */
 function search_func(helper) {
-  if(helper.state.query !== '') {
-    search_mode('on');
+  if (helper.state.query !== "") {
+    search_mode("on");
     helper.search();
 
     // TODO change style of search bar (blue, bigger)
   }
 }
-
 
 /**
  * Show / hide search elements
@@ -108,28 +108,27 @@ function search_func(helper) {
  * @param      on|off  search_is  Search status
  */
 function search_mode(search_is) {
-  var search_hits = document.getElementById('search-hits');
-  var body = document.getElementsByTagName('body')[0];
-  var cancel_search = document.querySelectorAll('#search .search-cancel')[0];
-  var search_class = 'search';
+  var search_hits = document.getElementById("search-hits");
+  var body = document.getElementsByTagName("body")[0];
+  var cancel_search = document.querySelectorAll("#search .search-cancel")[0];
+  var search_class = "search";
 
   // Turn search mode on
-  if(search_is == 'on') {
-    if(!body.classList.contains(search_class)) {
+  if (search_is == "on") {
+    if (!body.classList.contains(search_class)) {
       body.classList.add(search_class);
     }
-    search_hits.style.display = '';
-    cancel_search.style.display = 'block';
-  // Turn search mode off
+    search_hits.style.display = "";
+    cancel_search.style.display = "block";
+    // Turn search mode off
   } else {
-    if(body.classList.contains(search_class)) {
+    if (body.classList.contains(search_class)) {
       body.classList.remove(search_class);
     }
-    search_hits.style.display = 'none';
-    cancel_search.style.display = 'none';
+    search_hits.style.display = "none";
+    cancel_search.style.display = "none";
   }
 }
-
 
 /**
  * Clear text from the search input
@@ -139,19 +138,20 @@ function search_mode(search_is) {
 function clear_search(event) {
   event.preventDefault();
 
-  var search_input = document.querySelectorAll('#search .ais-SearchBox-input')[0];
-  search_input.value = '';
+  var search_input = document.querySelectorAll(
+    "#search .ais-SearchBox-input"
+  )[0];
+  search_input.value = "";
   baseUrl = window.location.href.split("?")[0];
-  window.history.pushState('', '', baseUrl);
+  window.history.pushState("", "", baseUrl);
 
   // Seems overkill
   // search.dispose()
   // search.addWidgets([searchBox, hits, pagination]);
   // search.start()
 
-  search_mode('off');
+  search_mode("off");
 }
-
 
 /**
  * Returns a formatted representation of the timestamp.
@@ -162,54 +162,55 @@ function clear_search(event) {
  * @return     Date|string    The formatted date (currently dd/mm/yyyy)
  */
 function get_locale_date_string_fallback(timestamp) {
-  if (typeof timestamp !== 'undefined') {
+  if (typeof timestamp !== "undefined") {
     var curr_date = new Date(timestamp);
     date_string = curr_date.toLocaleDateString();
 
     // TODO test fallback for older IE
-    if(!date_string) {
+    if (!date_string) {
       var curr_day = curr_date.getDate();
-      var curr_month = curr_date.getMonth() + 1 ;
+      var curr_month = curr_date.getMonth() + 1;
       var curr_year = curr_date.getFullYear();
-      date_string = curr_day + '/' + curr_month + '/' +  curr_year;
+      date_string = curr_day + "/" + curr_month + "/" + curr_year;
     }
   } else {
-    date_string = '';
+    date_string = "";
   }
 
-return date_string;
+  return date_string;
 }
 
 /**
  * Animate mobile menu
  */
 
-var menu_icon = document.querySelector('.menu-icon');
-var menu_overlay = document.querySelector('#menu-overlay');
+var menu_icon = document.querySelector(".menu-icon");
+var menu_overlay = document.querySelector("#menu-overlay");
 
 function toggle_menu() {
   // Toggle open mode
-  menu_icon.classList.toggle('open');
-  document.querySelector('nav#menu').classList.toggle('open');
-  document.querySelector('header').classList.toggle('open');
+  menu_icon.classList.toggle("open");
+  document.querySelector("nav#menu").classList.toggle("open");
+  document.querySelector("header").classList.toggle("open");
 
   // Prevent background body scrolling
-  document.querySelector('body').classList.toggle('navigation-open');
-  document.querySelector('html').classList.toggle('navigation-open');
+  document.querySelector("body").classList.toggle("navigation-open");
+  document.querySelector("html").classList.toggle("navigation-open");
 
   // Toggle overlay
-  menu_overlay.classList.toggle('visible');
+  menu_overlay.classList.toggle("visible");
 }
 
-menu_icon.addEventListener('click', toggle_menu);
+menu_icon.addEventListener("click", toggle_menu);
 
 /**
  * Hide header on scroll
  */
 
-var header = document.querySelector('.header-search');
+var header = document.querySelector(".header-search");
 
-var headroom  = new Headroom(header
+var headroom = new Headroom(
+  header
   //, {
   // Headroom config options
   // }
@@ -220,16 +221,23 @@ headroom.init();
  * Handle cookie bar
  */
 
-var cookie_bar = document.querySelector('#cookie-bar');
-var cookie_bar_cta = cookie_bar.getElementsByClassName('cta')[0];
+var cookie_bar = document.querySelector("#cookie-bar");
+var cookie_bar_cta = cookie_bar.getElementsByClassName("cta")[0];
 
-if (!Cookies.get('hide-cookie-consent-until') || Date.now() > Cookies.get('hide-cookie-consent-until')) {
-  cookie_bar.classList.add('visible');
-  cookie_bar_cta.addEventListener('click', function(e) {
+if (
+  !Cookies.get("hide-cookie-consent-until") ||
+  Date.now() > Cookies.get("hide-cookie-consent-until")
+) {
+  cookie_bar.classList.add("visible");
+  cookie_bar_cta.addEventListener("click", function (e) {
     e.preventDefault();
-    cookie_bar.classList.remove('visible');
-    Cookies.set('hide-cookie-consent-until', Date.now() + 365 * 24 * 3600 * 1000, {expires: 365});
-  })
+    cookie_bar.classList.remove("visible");
+    Cookies.set(
+      "hide-cookie-consent-until",
+      Date.now() + 365 * 24 * 3600 * 1000,
+      { expires: 365 }
+    );
+  });
 }
 
 /**
@@ -261,14 +269,14 @@ if (!Cookies.get('hide-cookie-consent-until') || Date.now() > Cookies.get('hide-
  * Masonry homepage
  */
 function renderMasonry() {
-  var grid = document.querySelector('.latest-content');
+  var grid = document.querySelector(".latest-content");
   if (grid !== null) {
     var msnry = new Masonry(grid, {
-    itemSelector: '.excerpt',
-    columnWidth: '.excerpt',
-    gutter: '.gutter-sizer',
-    percentPosition: true,
-    transitionDuration: 0,
+      itemSelector: ".excerpt",
+      columnWidth: ".excerpt",
+      gutter: ".gutter-sizer",
+      percentPosition: true,
+      transitionDuration: 0,
     });
   }
 }
